dace9587647c70b93e8db17df9c59cd0d5d7cf6a
ayush931 <ayushkumar9315983@gmail.com>
2025-07-16 00:33:23 +0530

    changes

--- src/main/java/com/example/demo/authentication/JwtUtil.java
+++ src/main/java/com/example/demo/authentication/JwtUtil.java
@@ -12,10 +12,10 @@ public class JwtUtil {
         String securityCode = "ayush@123456@123456@ayush@12345@ayush@123456";
         long expiration = 7 * 24 * 60 * 60 * 1000;
         return Jwts.builder()
-                .setSubject(userId)
+                .subject(userId)
                 .claim("role", role)
                 .issuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .expiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(SignatureAlgorithm.HS256, securityCode.getBytes())
                 .compact();
     }
